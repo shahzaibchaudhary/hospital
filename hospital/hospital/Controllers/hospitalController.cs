8a6e4ad2e743f2c598a3b445c73c6ca04753eea0
Mohsin675 <mohsin.bashir675@gmail.com>
2018-05-01 16:31:24 -0700

    Mohsin  add controller

---
+++ hospital/hospital/Controllers/hospitalController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hospital.Controllers
+{
+    public class hospitalController : Controller
+    {
+        // GET: hospital
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: hospital/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: hospital/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: hospital/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: hospital/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: hospital/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: hospital/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: hospital/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
